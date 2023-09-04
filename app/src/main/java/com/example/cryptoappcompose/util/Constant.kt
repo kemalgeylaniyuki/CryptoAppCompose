d83bfd69cfb551deb08827a6e30d765bf73ef197
kemal.yuki <kemalgeylaniyuki@gmail.com>
2023-09-04 13:58:56 +0300

    Project Created & Navigation

---
+++ app/src/main/java/com/example/cryptoappcompose/util/Constant.kt
@@ -0,0 +1,11 @@
+package com.example.cryptoappcompose.util
+
+object Constants {
+
+    //https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/main/cryptolist.json
+
+    const val BASE_URL = "https://raw.githubusercontent.com/"
+
+    //https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/main/crypto.json
+
+}
